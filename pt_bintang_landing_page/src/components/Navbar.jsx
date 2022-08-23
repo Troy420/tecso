894cc11f18b1b54664ea19bc6d79004d8644ea6e
Bernard <bernardy_ho@hotmail.com>
2022-08-23 23:18:13 +0700

    in progress navbar

--- pt_bintang_landing_page/src/components/Navbar.jsx
+++ pt_bintang_landing_page/src/components/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {useState} from 'react';
+
+import {close, menu} from '../assets';
+import logoBintang from '../assets/logoBintang.png';
+import {navLinks} from '../constants';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logoBintang} alt="asdf" className='h-[100px]'/>
+
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li 
+            key={nav.id}
+            className={`font-poppins font-normal custor-pointer text-[16px] text-white mr-10`}
+          >
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
